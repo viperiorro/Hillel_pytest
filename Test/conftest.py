1b7805be9c30486cf1545ca52aac9854a268072b
Yepavlov <ff2-15@ukr.net>
2023-07-31 11:28:38 +0200

    The second changes with added scope.

--- Test/conftest.py
+++ Test/conftest.py
@@ -8,12 +8,12 @@ from app.to_do_list import ToDoList
 def task_object():
     return Task("Test", "Create test", "2023-07-30", "High")
 
-@pytest.fixture()
+@pytest.fixture(scope="class")
 def task_object2():
     return Task("Test2", "Create test2", "2023-08-25", "Low")
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture()
 def create_to_do_list():
     return ToDoList()
 
